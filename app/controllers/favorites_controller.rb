344819661c793c30d2914dc85d4f03220a4f73da
shoyamauchi <yonakiart@gmail.com>
2022-06-01 05:21:27 +0900

    index,search,mytabiにphotoを追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,13 @@ class FavoritesController < ApplicationController
   # お気に入り登録
   def create
     @favorite = Favorite.create(user_id: current_user.id, tabi_id: params[:id])
-    redirect_to root_path
+    redirect_to index_tabis_path
   end
   # お気に入り削除
   def destroy
     @favorite = Favorite.find_by(tabi_id: current_user.id, tabi_id: params[:id])
     @favorite.destroy
-    redirect_to root_path
+    redirect_to index_tabis_path
   end
 
 end
